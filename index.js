11f1378194538864c6f8e00267956cba262676a4
Andrei Savelyeu <raikaat@mail.ru>
2018-10-28 16:23:18 +0300

    feat: add sumOfOther function

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const sumOfOther = (arr) => {
+  let total = 0;
+  const newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    total += +arr[i];
+  }
+  for (let j = 0; j < arr.length; j++) {
+    newArr.push(total - arr[j]);
+  }
+  return newArr;
+};
 
+exports.sumOfOther = sumOfOther;
